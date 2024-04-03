2419aa0f7f077ec7661dd0079589b00c4245b28f
MatheusAndrade <msmatheus14@gmail.com>
2024-04-02 21:36:11 -0400

    Trabalho renato quase finalizado

--- app.js
+++ app.js
@@ -1,10 +1,76 @@
-const express = require ('express')
+
+//Ao testar as requições da API no postman, favor use x-www-form-urlencoded
+
+
+
+
+const express = require('express')
 const app = express()
 const porta = 3000
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
+
+
+const vagas = []
+
+//Iniciei o programa com 3 vagas nativas.
+adicionarVaga(1, "Desenvolvedor Frontend", ["HTML", "CSS", "JavaScript"], 5000, ["Plano de saúde", "Vale refeição"], "ativo");
+adicionarVaga(2, "Desenvolvedor Backend", ["Node.js", "Express", "MongoDB"], 6000, ["Plano de saúde", "Vale alimentação"], "ativo");
+adicionarVaga(3, "Analista de Dados", ["SQL", "Python", "Machine Learning"], 7000, ["Plano de saúde", "Bônus anual"], "ativo");
+
+
+app.get('/vagas', (req,res) => {
+    res.render('index', {vagas: vagas})
+})
+
+
+app.get('/vagas/:id', (req,res) => {
+
+    const id = parseInt (req.params.id)
+    const vaga = vagas.find(vaga => vaga.id === id)
+
+    if(!vaga) {
+        //Adicionei um emoji triste quando o sistema não encontra uma vaga com o ID.
+        res.status(404).send('<h1> Vaga não encontrada </h1> <img src="https://images.emojiterra.com/twitter/512px/1f641.png">');
+
+    }
+    else
+    {
+        res.status(200)
+        res.render('vaga-busca', {vaga: vaga})
+    }
+
+    
+})
+
+
+app.post('/vagas', (req, res) => {
+
+    const { id, titulo, conhecimentos, remuneracao, beneficios, status } = req.body
+let output = {
+    id: id,
+    titulo:titulo,
+    conhecimentos: conhecimentos,
+    remuneracao: remuneracao,
+    beneficios: beneficios, 
+    status: status
+}
+
+    if(adicionarVaga(id, titulo, conhecimentos, remuneracao, beneficios,status))
+    {
+        res.status(200)
+        res.render('vaga-adicionada', {vaga: output})
+    }
+    else{
+
+        res.status(500)
+    }
+    
+
+    
+});
 
 
 
@@ -20,59 +86,47 @@ function adicionarVaga(id,titulo,conhecimentos,remuneracao,beneficios,status){
         status:status
     }
     
-    vagas.push(vaga)
+    if(vagas.push(vaga)){
+        return true
+    }else
+    {
+        return false
+    }
 }
 
-const vagas = []
 
-adicionarVaga(1, "Desenvolvedor Frontend", ["HTML", "CSS", "JavaScript"], 5000, ["Plano de saúde", "Vale refeição"], "ativo");
-adicionarVaga(2, "Desenvolvedor Backend", ["Node.js", "Express", "MongoDB"], 6000, ["Plano de saúde", "Vale alimentação"], "ativo");
-adicionarVaga(3, "Analista de Dados", ["SQL", "Python", "Machine Learning"], 7000, ["Plano de saúde", "Bônus anual"], "ativo");
+app.put('/vagas', (req, res) => {
 
+    const { id, titulo, conhecimentos, remuneracao, beneficios, status } = req.body;
 
-app.get('/vagas', (req,res) => {
-    res.render('index', {vagas: vagas})
-})
+    let output = {
 
-
-//Implementei um ID que é passado como parâmetro para ele a Vaga com ID específico.
-app.get('/vagas/:id', (req,res) => {
-
-    const id = parseInt (req.params.id)
-    const vaga = vagas.find(vaga => vaga.id === id)
-
-    if(!vaga) {
-
-        res.status(404).send('<h1> ID não encontrado </h1> <img src="https://images.emojiterra.com/twitter/512px/1f641.png">');
-
+        id:id, 
+        titulo: titulo,
+        conhecimentos: conhecimentos,
+        remuneracao: remuneracao,
+        beneficios: beneficios,
+        status: status
     }
-    else
-    {
+    
+    console.log(id)
+
+     if(atualizarvaga(id, titulo, conhecimentos, remuneracao, beneficios, status))
+     {
         res.status(200)
-        res.render('vaga', {vaga: vaga})
-    }
-
-    
-})
-
-app.get('/criar-vaga', (req, res) => {
-
-    res.render('criarvaga');
-});
-
-
-app.post('/criar-vaga', (req, res) => {
-
-    const { id, titulo, conhecimentos, remuneracao, beneficios, status } = req.body
-
-    adicionarVaga(id, titulo, conhecimentos.split(','), remuneracao, beneficios.split(','), status);
-
-    res.redirect('/vagas');
-    
+        res.render('vaga-atualizada', {vaga:output})
+     }
+     else
+     {
+        res.status(404)
+        res.send('<h1>ERROR 404, NOT FOUND!</h1>')
+        
+     }
+     
 });
 
 function atualizarvaga(id, titulo, conhecimentos, remuneracao, beneficios, status)
- {
+{
     let encontrado = false
 
     for (let i = 0; i < vagas.length; i++)
@@ -82,9 +136,9 @@ function atualizarvaga(id, titulo, conhecimentos, remuneracao, beneficios, statu
             encontrado = true
             
             vagas[i].titulo = titulo;
-            vagas[i].conhecimentos = conhecimentos.split(', ');
+            vagas[i].conhecimentos = conhecimentos;
             vagas[i].remuneracao = remuneracao;
-            vagas[i].beneficios = beneficios.split(', ');
+            vagas[i].beneficios = beneficios;
             vagas[i].status = status;
 
             
@@ -102,22 +156,41 @@ function atualizarvaga(id, titulo, conhecimentos, remuneracao, beneficios, statu
     }
 }
 
-app.put('/atualizar-vaga/:id/', (req, res) => {
 
-    const { id, titulo, conhecimentos, remuneracao, beneficios, status } = req.body;
+
+app.delete('/vagas', (req,res) => {
+    const id = req.query.id
+    let encontrado = false
+
+    let vagaExcluida = []
+
+
+   for(let i = 0; i < vagas.length; i++){
+
+        if(vagas[i].id == parseInt(id)){
+
+            vagaExcluida.push(vagas[i])
+            vagas.splice(i,1)
+            encontrado = true
+            
+        }
+    }
+
     
-     if(atualizarvaga(id, titulo, conhecimentos, remuneracao, beneficios, status))
-     
-
-    res.redirect('/vagas');
-
-});
+    if(encontrado == true){
+    res.status(200)
+    res.render('vaga-excluida', {vagas:vagaExcluida})
+    }
+    else
+    {
+        res.status(404)
+        res.send('<h1>VALOR NÃO ENCONTRADO</h1>')
+    }
+    
+})
 
 
 app.listen(porta, ()=> {
 
     console.log('Servidor rodando na porta 3000')
 })
-
-
- 
